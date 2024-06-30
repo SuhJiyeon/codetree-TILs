1f99a1ea868bb1470501dc7634f568ef046f0f72
SuhJiyeon <113087916+SuhJiyeon@users.noreply.github.com>
2024-06-30 17:07:34 +0900

    Title: 세 정수형 변수 선언, Time: 112ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240630/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a=5;
+        System.out.printf("A is %d\n",a);
+
+        String b="apple";
+        System.out.printf("B is %s\n",b);
+
+        System.out.printf("A is %d and B is %s\n", a, b);
+    }
+}
